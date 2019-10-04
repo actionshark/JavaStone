44683e0460ce43e569f558650ae78ddddb41bd7c
actionshark <sunhongkao@qq.com>
2019-10-04 09:57:47 +0800

    Math.clamp

--- src/com/shk/js/math/Math.java
+++ src/com/shk/js/math/Math.java
@@ -136,4 +136,52 @@ public class Math {
 
 		return sum;
 	}
+	
+	public static int clamp(int value, int min, int max) {
+		if (value < min) {
+			return min;
+		}
+		
+		if (value > max) {
+			return max;
+		}
+		
+		return value;
+	}
+	
+	public static long clamp(long value, long min, long max) {
+		if (value < min) {
+			return min;
+		}
+		
+		if (value > max) {
+			return max;
+		}
+		
+		return value;
+	}
+	
+	public static float clamp(float value, float min, float max) {
+		if (value < min) {
+			return min;
+		}
+		
+		if (value > max) {
+			return max;
+		}
+		
+		return value;
+	}
+	
+	public static double clamp(double value, double min, double max) {
+		if (value < min) {
+			return min;
+		}
+		
+		if (value > max) {
+			return max;
+		}
+		
+		return value;
+	}
 }
